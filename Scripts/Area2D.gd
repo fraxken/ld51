f7c7dcd51abe0c2121024696f1a02874505fc67f
Thomas GENTILHOMME <gentilhomme.thomas@gmail.com>
2022-09-30 17:26:04 +0200

    chore: continue to work prototype

--- Scripts/Area2D.gd
+++ Scripts/Area2D.gd
@@ -3,4 +3,4 @@ extends Area2D
 func _on_Area2D_body_entered(body: PhysicsBody2D) -> void:
 	print("hello world!")
 	print(body)
-	queue_free()
+	#queue_free()
--- Scripts/Player2D.gd
+++ Scripts/Player2D.gd
@@ -3,8 +3,10 @@ extends KinematicBody2D
 export (int) var speed = 100
 export (int) var jump_speed = -200
 export (int) var gravity = 280
+export (int) var max_jump = 1
 
 var velocity = Vector2.ZERO
+var jumpCount: int = 0
 
 func get_input():
 	velocity.x = 0
@@ -17,14 +19,17 @@ func get_input():
 
 func _physics_process(delta):
 	get_input()
-	#if velocity.x == 0:
-	#	$AnimationPlayer.play("Idle")
-	#else:
-	#	$AnimationPlayer.play("Walk")
+	if velocity.x == 0:
+		$AnimationPlayer.play("Idle")
+	else:
+		$AnimationPlayer.play("Walk")
 	velocity.y += gravity * delta
 	velocity = move_and_slide(velocity, Vector2.UP)
 	
 	if Input.is_action_just_pressed("jump"):
 		if is_on_floor():
 			velocity.y = jump_speed
-
+			jumpCount = 0
+		elif jumpCount < max_jump:
+			velocity.y = jump_speed
+			jumpCount += 1
