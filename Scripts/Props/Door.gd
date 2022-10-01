19b0e0e44470b541f2fc5e371b9872a56967145d
Gentilhomme <gentilhomme.thomas@gmail.com>
2022-10-01 20:10:57 +0200

    feat: shakescreen & door/lever fix (#8)

--- Scripts/Props/Door.gd
+++ Scripts/Props/Door.gd
@@ -1,8 +1,8 @@
 extends Node2D
 
 export (NodePath) var ActionableItemPath = null
-export (int) var move_time = 3.0
-export (int) var opening_delay = 0.5
+export (int) var move_time = 1.5
+export (int) var opening_delay = 0.2
 export (Vector2) var move_to = Vector2.DOWN * 96
 
 onready var door = $Static
@@ -19,11 +19,14 @@ func _physics_process(delta):
 	door.position = door.position.linear_interpolate(follow, 0.075)
 
 func _open(state):
-	# TODO: fix teleportation bug with tween
+	tween.stop_all()
+	
 	if state:
 		tween.interpolate_property(self, "follow", Vector2.ZERO, move_to, move_time, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT, opening_delay)
 		tween.start()
 	else:
 		tween.interpolate_property(self, "follow", move_to, Vector2.ZERO, move_time, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT, opening_delay)
 		tween.start()
-		
+
+func _on_MoveTween_tween_all_completed():
+	targetNode.unlock()
