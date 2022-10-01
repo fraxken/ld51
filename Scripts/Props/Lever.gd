ac8a0d655084b68b2878493ee68043a220543a01
Thomas GENTILHOMME <gentilhomme.thomas@gmail.com>
2022-10-01 15:28:09 +0200

    refactor: enhance door & lever mechanism

--- Scripts/Props/Lever.gd
+++ Scripts/Props/Lever.gd
@@ -2,12 +2,12 @@ extends Area2D
 
 signal lever_turned(state)
 
-export (int) var delay = 2.0
+export (bool) var state = false
+export (int) var delay = 1.0
 
 onready var timer = $Timer
 
 var playerOnLever = false
-var leverEnabled = false
 var leverUsable = true
 
 const COLORS: Dictionary = {
@@ -16,6 +16,7 @@ const COLORS: Dictionary = {
 }
 
 func _ready():
+	_renderToCurrentState()
 	timer.set_wait_time(delay)
 	timer.connect("timeout", self, "_on_Timer_timeout")
 
@@ -23,16 +24,18 @@ func _input(event):
 	if event.is_action_pressed("use"):
 		_trigger_lever()
 		
+func _renderToCurrentState():
+	var newCurrentState = "enabled" if state else "disabled"
+	var color = COLORS[newCurrentState]
+	$Sprite.color = color
+		
 func _trigger_lever():
 	if playerOnLever && leverUsable:
-		var state = "disabled" if leverEnabled else "enabled"
-		var color = COLORS[state]
-		
-		$Sprite.color = color
-		leverEnabled = not leverEnabled
+		state = not state
+		_renderToCurrentState()
 		leverUsable = false
 		timer.start()
-		emit_signal("lever_turned", leverEnabled)
+		emit_signal("lever_turned", state)
 
 func _on_Lever_body_entered(body):
 	playerOnLever = true
