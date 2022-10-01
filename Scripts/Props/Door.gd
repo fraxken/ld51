46503333d39ed89720634d796e00b65fc6bacdb9
Thomas GENTILHOMME <gentilhomme.thomas@gmail.com>
2022-10-01 15:28:09 +0200

    refactor: enhance door & lever mechanism

--- Scripts/Props/Door.gd
+++ Scripts/Props/Door.gd
@@ -1,28 +1,29 @@
 extends Node2D
 
+export (NodePath) var ActionableItemPath = null
 export (int) var move_time = 3.0
-export (int) var opening_delay = 2.0
+export (int) var opening_delay = 0.5
 export (Vector2) var move_to = Vector2.DOWN * 96
 
 onready var door = $Static
 onready var tween = $MoveTween
 
 var follow = Vector2.ZERO
+var targetNode: Node2D
 
 func _ready():
-	_init_tween()
-	
-func _init_tween():
-	tween.interpolate_property(self, "follow", Vector2.ZERO, move_to, move_time, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT, opening_delay)
-	#tween.interpolate_property(self, "follow", move_to, Vector2.ZERO, move_time, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT, move_time + idle_duration * 2)
-	
+	targetNode = get_node(ActionableItemPath)
+	targetNode.connect("lever_turned", self, "_open")
+
 func _physics_process(delta):
 	door.position = door.position.linear_interpolate(follow, 0.075)
 
-func _open():
-	print("open door!")
-	tween.start()
-
-func _on_Lever_lever_turned(state):
-	print("lever activated!")
-	_open()
+func _open(state):
+	# TODO: fix teleportation bug with tween
+	if state:
+		tween.interpolate_property(self, "follow", Vector2.ZERO, move_to, move_time, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT, opening_delay)
+		tween.start()
+	else:
+		tween.interpolate_property(self, "follow", move_to, Vector2.ZERO, move_time, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT, opening_delay)
+		tween.start()
+		
