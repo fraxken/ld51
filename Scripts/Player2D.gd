f499bc7eb94daa12fa0d5e663a4a86bb7dfead5f
Thomas GENTILHOMME <gentilhomme.thomas@gmail.com>
2022-10-03 01:56:31 +0200

    refactor: fraxken levels

--- Scripts/Player2D.gd
+++ Scripts/Player2D.gd
@@ -2,7 +2,7 @@ extends KinematicBody2D
 
 export (int) var speed = 50
 export (int) var max_speed = 150
-export (int) var jump_speed = 300
+export (int) var jump_speed = 270
 export (int) var max_jump = 1
 
 onready var gravityTimer = $GravityTimer
