6f78219572a6ea0c75ed3de17c905f4fca4b5f99
im_codebreaker <53506859+im-codebreaker@users.noreply.github.com>
2022-10-02 14:27:34 +0200

    Ft/add pause gui (#16)
    * chore: add pause menu to Interface Scene, remove useless scripts
    
    * chore: remove PauseMenu test scene

--- Scripts/UI/PauseMenu.gd
+++ Scripts/UI/PauseMenu.gd
@@ -1,13 +1,18 @@
 extends Control
 
-onready var set_tree = get_tree()
 func _input(event):
 	if event.is_action_pressed("pause"):
 		var state = not get_tree().paused
 		get_tree().paused = state
 		visible = state
 
-
-func _on_ResumeButton_game_resume():
+func _on_ResumeButton_pressed():
 	get_tree().paused = false
 	visible = false
+
+func _on_RestartButton_pressed():
+	get_tree().paused = false
+	SceneChanger.next_scene("res://Scenes/UI/StartScreen.tscn")
+
+func _on_ExitButton_pressed():
+	get_tree().quit()
