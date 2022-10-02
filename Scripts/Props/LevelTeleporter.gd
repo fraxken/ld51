14c54ebe167fb3683e105491a1c51863e4c454ed
Thomas GENTILHOMME <gentilhomme.thomas@gmail.com>
2022-10-03 00:21:24 +0200

    chore: add teleport door

--- Scripts/Props/LevelTeleporter.gd
+++ Scripts/Props/LevelTeleporter.gd
@@ -1,11 +1,20 @@
 extends Area2D
 
 export (String) var target_scene
+export (bool) var state = true
+
+onready var animPlayer = $AnimationPlayer
 
 var playerEntered = false
 var usedOnce = false
+
+func _ready():
+	animPlayer.play_backwards("Open")
 	
 func _input(event):
+	if !state:
+		return
+		
 	if playerEntered && event.is_action_pressed("use") && usedOnce == false:
 		usedOnce = true
 		if !target_scene:
@@ -16,9 +25,11 @@ func _input(event):
 		SceneChanger.next_scene(target_scene)
 
 func _on_LevelTeleporter_body_entered(body):
-	if body.is_in_group("player"):
+	if state && body.is_in_group("player"):
 		playerEntered = true
+		animPlayer.play("Open")
 
 func _on_LevelTeleporter_body_exited(body):
-	if body.is_in_group("player"):
+	if state && body.is_in_group("player"):
 		playerEntered = false
+		animPlayer.play_backwards("Open")
