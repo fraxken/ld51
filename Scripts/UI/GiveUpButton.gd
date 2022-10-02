57b56fcc0805d1ccf0f6e902097f2753be6ffd10
MehdiHan <meh.bouchard@outlook.fr>
2022-10-02 14:24:36 +0200

    chore: add pause menu to Interface Scene, remove useless scripts

--- Scripts/UI/GiveUpButton.gd
+++
@@ -1,5 +0,0 @@
-extends Button
-
-func _on_GiveUpButton_pressed():
-	get_tree().paused = false
-	SceneChanger.next_scene("res://Scenes/UI/StartScreen.tscn")
--- Scripts/UI/ResumeButton.gd
+++
@@ -1,7 +0,0 @@
-extends Button
-
-signal game_resume()
-
-func _on_ResumeButton_pressed():
-	if get_tree().paused == true: 
-		emit_signal("game_resume")
