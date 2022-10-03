d060b442f3c4a71f6885d7079abfbaffacf3218b
Thomas GENTILHOMME <gentilhomme.thomas@gmail.com>
2022-10-03 14:36:03 +0200

    chore: add frax_B

--- Box.gd
+++ Box.gd
@@ -2,7 +2,12 @@ extends KinematicBody2D
 
 class_name Box
 
+onready var original_position = global_position
+
 var velocity = Vector2.ZERO
+
+func reset_initial():
+	position = original_position
 
 func _physics_process(delta):
 	move_and_slide(velocity, Vector2.UP)
