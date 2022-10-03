26a3a8e50a3f9175f37ee64abafe034f9ec0c022
rossb0b <nicolas.hallaert.pro@gmail.com>
2022-10-03 21:04:00 +0200

    fix: in repeat mode false, only re-activate when an animation is already running

--- Scripts/Props/FallingTrap.gd
+++ Scripts/Props/FallingTrap.gd
@@ -11,23 +11,34 @@ onready var fallingTrap = $Static
 onready var tween = $MoveTween
 onready var collider = $FreeArea/CollisionShape2D
 
+var inMovement = false
 var follow = Vector2.ZERO
 var targetNode: Node2D
 
 func _ready():
 	targetNode = get_node(ActionableItemPath)
-	targetNode.connect("reactiveArea_turned", self, "_open")
 	if repeat == true:
+		targetNode.connect("reactiveArea_turned", self, "_open", [], CONNECT_ONESHOT)
 		tween.set_deferred("repeat", true)
+	else:
+		targetNode.connect("reactiveArea_turned", self, "_open")
 
 func _physics_process(delta):
 	fallingTrap.position = fallingTrap.position.linear_interpolate(follow, 0.075)
 
 func _open(state):
-	tween.interpolate_property(self, "follow", Vector2.ZERO, move_to, move_time, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT, opening_delay)
-	tween.interpolate_property(self, "follow", move_to, Vector2.ZERO, move_time, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT, move_time + idle_duration * 2)
-	tween.start()
+	if repeat == false && inMovement == true:
+		pass
+	else:
+		inMovement = true
+		tween.interpolate_property(self, "follow", Vector2.ZERO, move_to, move_time, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT, opening_delay)
+		tween.interpolate_property(self, "follow", move_to, Vector2.ZERO, move_time, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT, move_time + idle_duration * 2)
+		tween.start()
 	
 func _on_FreeArea_body_entered(body):
 	if body.is_in_group("player"):
 		Globals.controller.teleportPlayerToStartPosition(true, "falling trap")
+
+func _on_MoveTween_tween_all_completed():
+	if repeat == false:
+		inMovement = false
